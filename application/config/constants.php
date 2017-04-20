7287315c875f797e1cfeeee0f3043eb61340bfbc
Mr. Gabor Eszaki <mr.gabor.eszaki@gmail.com>
2017-04-20 17:08:02 +0200

    FrontEnd Update & structure update

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,10 +84,10 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-defined('CSS_path')      OR define('CSS_path', "/application/assets/css/"); //CONSTANS CSS loading path
-defined('IMG_path')      OR define('IMG_path', "/application/assets/images/"); //CONSTANS Images loading path
-defined('IMG_path_absolute')      OR define('IMG_path_absolute', APPPATH."/assets/images/"); //CONSTANS Images loading path
-defined('JS_path')      OR define('JS_path', "/application/assets/js/"); //CONSTANS Images loading path
+defined('CSS_path')      OR define('CSS_path', "/assets/css/"); //CONSTANS CSS loading path
+defined('IMG_path')      OR define('IMG_path', "/assets/images/"); //CONSTANS Images loading path
+defined('IMG_path_absolute')      OR define('IMG_path_absolute', "assets/images/"); //CONSTANS Images loading path
+defined('JS_path')      OR define('JS_path', "/assets/js/"); //CONSTANS Images loading path
 
 defined('img_champ_lib')        OR define('img_champ_lib', IMG_path_absolute."championicons/");
 defined('img_champ_id')         OR define('img_champ_id', img_champ_lib."id/"); 
